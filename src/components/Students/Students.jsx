def29eaa7e94c21bbdb93d45db04fe0e2b20f196
Saidumarov <saidumarovjafarxon@gmail.com>
2024-04-04 22:28:57 +0500

    .

--- src/components/Students/Students.jsx
+++ src/components/Students/Students.jsx
@@ -26,6 +26,7 @@ export default function Students() {
   };
 
   //
+
   useEffect(() => {
     fetchData();
   }, []);
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,4 @@
-import React from "react";
-
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Header from "../components/Header";
 import Teachers from "../components/Teachers/Teachers";
 import Students from "../components/Students/Students";
 import Profile from "../components/Profile/Profile";
@@ -11,6 +8,7 @@ import StudentsEdit from "../components/StudentsEdit/StudentsEdit";
 import StudentsAdd from "../components/StudentsAdd/StudentsAdd";
 import LoginPanel from "../components/Login/Login";
 import { useEffect, useState } from "react";
+import Sitebar from "../components/SiteBar_and_Header";
 
 const Router = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -32,16 +30,17 @@ const Router = () => {
   }, [isLogin, parms]);
   return (
     <>
-      {isLogin ? <Header /> : ""}
       <Routes>
-        <Route path="/" element={<Teachers />} />
-        <Route path="/students" element={<Students />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<LoginPanel login={setIsLogin} />} />
-        <Route path="/teachers/add" element={<TeachersAdd />} />
-        <Route path="/students/add" element={<StudentsAdd />} />
-        <Route path="/teachers/edit/:id" element={<TeachersEdit />} />
-        <Route path="/students/edit/:id" element={<StudentsEdit />} />
+        <Route path="/" element={<Sitebar />}>
+          <Route index element={<Teachers />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/teachers/add" element={<TeachersAdd />} />
+          <Route path="/students/add" element={<StudentsAdd />} />
+          <Route path="/teachers/edit/:id" element={<TeachersEdit />} />
+          <Route path="/students/edit/:id" element={<StudentsEdit />} />
+        </Route>
       </Routes>
     </>
   );
